6e759179f24467bcac115ac130574a53789585e2
russiantech <chrisjsmez@gmail.com>
2024-05-06 01:37:13 +0000

    read-me, and some annotated vars

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """Add two numbers and return the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
+
+if __name__ == "__main__":
+    print(add(1.11, 2.22) == 1.11 + 2.22)
+    print(add.__annotations__)
+
