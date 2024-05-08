13f1c9f709fa859c7f6a27cb52610713dee126e2
russiantech <chrisjsmez@gmail.com>
2024-05-08 01:48:42 +0000

    fixed pycodstyles

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -12,8 +12,3 @@ def add(a: float, b: float) -> float:
         float: The sum of a and b.
     """
     return a + b
-
-
-if __name__ == "__main__":
-    print(add(1.11, 2.22) == 1.11 + 2.22)
-    print(add.__annotations__)
